100cc330848bd8a970f9a02ca7cedbd5cae31fc7
Deepti931 <deeptishikha1999@gmail.com>
2022-08-16 21:48:00 +0530

    :sparkles:scripting started

---
+++ Browser/jira_clone/script.js
@@ -0,0 +1,23 @@
+let addBtn=document.querySelector(".add-btn");
+console.log(addBtn);
+
+var isModalPresent = false;
+addBtn.addEventListener("click", function() {
+    // case 1-> if screen is not present
+    //          then display modal
+
+if(!isModalPresent){
+    // display modal
+}
+
+    // case 2-> if modal is present
+    //          then hide modal
+
+else{
+    // display none
+}
+
+
+isModalPresent= !isModalPresent;
+
+})
